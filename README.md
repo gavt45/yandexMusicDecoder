ad138ee18db2c8773584fc981bdd8ef9fa7a255c
Gumirov Andrey <andreyt45@gmail.com>
2020-07-17 20:55:54 +0700

    Added naming feature

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Android cached yandex music decoder.
 Usually music files are stored in `/sdcard/Android/data/ru.yandex.music/files/<...>`
 
 ## WARNING!!! Use only for educational purposes. Do not publish decoded tracks! This actions are violating the license agreement!!! 
+
+
+
+curl 'https://music.yandex.ru/api/v2.1/handlers/tracks?tracks=631623:69842&external-domain=music.yandex.ru&overembed=no' -H 'Connection: keep-alive' -H 'Accept: application/json; q=1.0, text/*; q=0.8, */*; q=0.1' -H 'X-Requested-With: XMLHttpRequest' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0' -H 'X-Retpath-Y: https%3A%2F%2Fmusic.yandex.ru%2F'
+-H 'Referer: https://music.yandex.ru/album/1337/track/1337' -H 'Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7' 
+-H 'Sec-Fetch-Mode: cors' 
+ -H 'Sec-Fetch-Site: same-origin'
+-H 'Sec-Fetch-Dest: empty'
+
+
+-H 'Cookie: yandexuid=4164165061589644315; yuidss=4164165061589644315; ymex=1906723743.yrts.1591363743; _ym_uid=1591363743826884788; _ym_d=1591363743; mda=0; my=YwA=; cycada=J03AlwvwiyNnf88v7ZCSDq659zLl0+bYz7pB/qpmnRE=; yabs-frequency=/5/200002CPxbu00000/; L=RUBFU0QEfVxGSFoNYWVgBABOD0h9UgdyASdTBVo=.1592841566.14273.373340.b24d4f8de758a1ee237344d220c3a23a; device_id="b2c073114152541047c870afb5d826b14102abac3"; i=6kL/UorEFi7lU7m5hdAtUEsfn4aC9bANVTcaiGwFJhRGB5XwOCf/ktVZn4Me4pOGCPlwMReY34d0V59DkPkb1W46R2I=; yp=1907767074.sad.1592407074%3A1592407074%3A1#1908201631.multib.1#1593673188.szm.2%3A1680x1050%3A1680x844#1593673464.zlgn_smrt.1#1593324950.ln_tp.01; ys=wprid.1593238547506492-1748814652654577490100303-production-app-host-vla-web-yp-350; active-browser-timestamp=1593238777545'
--- decoder.py
+++ decoder.py
@@ -1,9 +1,46 @@
 import os,sys
+from re import findall,sub,match
+import requests
+
+def get_info(idd):
+    headers = {
+        'Connection': 'keep-alive',
+        'Accept': 'application/json; q=1.0, text/*; q=0.8, */*; q=0.1',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0',
+        'X-Retpath-Y': 'https%3A%2F%2Fmusic.yandex.ru%2F',
+    }
+
+    params = (
+        ('tracks', str(idd)),
+        ('external-domain', 'music.yandex.ru'),
+        ('overembed', 'no'),
+    )
+
+    resp = requests.get('https://music.yandex.ru/api/v2.1/handlers/tracks', headers=headers, params=params)
+    json = resp.json()
+    return ['{} - {}'.format(json[i]['artists'][0]['name'],json[i]['title']) for i in range(len(idd.split(',')))] 
+
+# print(get_info('112314,18385776,61835292,24327733'))
+# exit(0)
 
 def main(files_path):
-    for f in os.listdir(files_path):
-        print("Processing {}...".format(f), end='')
+    if files_path.endswith('/'):
+        files_path = files_path[:-1]
+    if not os.path.exists(files_path+"-decoded"):
+        os.mkdir(files_path+"-decoded")
+    files = os.listdir(files_path)
+    files_count = len(files)
+    for i, f in enumerate(files):
+        stat_msg = "\rProcessing %s%s: [%s>%s] %.2f%%" % (
+                                '0'*(8-len(f)),
+                                f,
+                                '='*int(float(i+1)/float(files_count)*30.0),
+                                '.'*int(30.0-float(i+1)/float(files_count)*30.0-1),
+                                float(i+1)/float(files_count)*100.0
+                            )
         fle = open(files_path+"/"+f, 'rb')
+        sz = os.stat(files_path+"/"+f).st_size
         b = fle.read(1)[0]
         b2 = fle.read(1)[0]
         flag10 = False
@@ -14,17 +51,56 @@ def main(files_path):
             b = (~b & 0xFF)
         flag12 = not flag10
         if flag12:
-            fileStream2 = open(files_path+"/{}.m4a".format(f), 'ab')
+            fileStream2 = open("{}-decoded/{}.m4a".format(files_path,f), 'ab')
         else:
-            fileStream2 = open(files_path+"/{}.mp3".format(f), 'ab')
+            fileStream2 = open("{}-decoded/{}.mp3".format(files_path,f), 'ab')
         fle.seek(0)
+        read_sz = 0
         array2 = fle.read(16384)
         while len(array2) > 0:
+            read_sz += len(array2)
+            print(stat_msg + " file decoding progress: [%s>%s] %.2f%%   " % ('='*int(float(read_sz)/float(sz)*30.0), '.'*int(29.0 - float(read_sz)/float(sz)*30.0), float(read_sz)/float(sz)*100.0),end='')
             fileStream2.write(bytes([e^b for e in array2]))
             array2 = fle.read(16384)
-        print('done!')
+        # print('...done!',end)
+def resolve(files_path):
+    if files_path.endswith('/'):
+        files_path = files_path[:-1]
+    if not os.path.exists(files_path+"-decoded"):
+        print("{}-decoded folder not found!".format(files_path))
+        return
+    files_path += "-decoded"
+    files = os.listdir(files_path)
+    files_count = len(files)
+    for i in range(0,files_count,10):
+        print("\rProcessing [%s>%s] %.2f%%" % (
+            '='*int(float(i+1)/float(files_count)*30.0),
+            '.'*int(30.0-float(i+1)/float(files_count)*30.0-1),
+            float(i+1)/float(files_count)*100.0
+        ), end='')
+        chunk = files[i:i+10]
+        _chunk = []
+        for part in chunk:
+            if match(r"[0-9]+\.(m4a|mp3)", part):
+                _chunk.append(part)
+        del chunk
+        ids = ','.join([part.replace(".m4a","").replace(".mp3","") for part in _chunk])
+        results = get_info(ids)
+        # print(results)
+        # print(chunk)
+        for i,fle in enumerate(_chunk):
+            new_name = sub(r"[0-9]+\.m",results[i]+".m",fle)
+            # print("new name: "+new_name)
+            os.rename(files_path + "/" + fle, files_path + "/" + new_name.replace('/','|').replace('\\','|'))
+        # return
+
+
 if len(sys.argv) < 2:
     print("Usage: "+sys.argv[0]+" <dir with encoded files>")
     exit(1)
 else:
     main(sys.argv[1])
+    inp = input('Start resolvment process? [Y/n]')
+    if inp == 'y' or inp == 'yes' or inp == '':
+        print("Starting resolvment process...")
+        resolve(sys.argv[1])
